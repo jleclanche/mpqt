7d6a38e120108815844494cf931898110200aaee
Jerome Leclanche <jerome@leclan.ch>
2011-02-05 13:44:02 +0000

    Improve MimeType, move registration to class method MimeType.register(), add MimeType.registerExtension(), add methods genericIcon, icon, isDefault and parent, register parent and generic icon as well and improve other stuff

--- parsemime.py
+++ parsemime.py
@@ -8,48 +8,84 @@ tpl = """# -*- coding: utf-8 -*-
 import os.path
 from mimetypes import types_map
 
-comments_map = %r
-
 DEFAULT_MIME_TYPE = "application/octet-stream"
 
-def register(mime, extension, comment=""):
-	types_map[extension] = mime
-	comments_map[mime] = comment
-
-register("application/vnd.bliz-anim", ".anim", "Model ANIM data")
-register("application/vnd.bliz-dbc",  ".dbc",  "WoW database cache")
-register("application/vnd.bliz-db2",  ".db2",  "WoW database cache 2")
-register("application/vnd.bliz-mdx",  ".mdx",  "MDX model")
-register("application/vnd.bliz-mdx2", ".m2",   "MDX2 model")
-register("application/vnd.bliz-mdx3", ".m3",   "MDX3 model")
-register("application/vnd.bliz-mpq",  ".mpq",  "MPQ archive")
-register("application/vnd.bliz-skin", ".skin", "Model SKIN data")
-register("application/vnd.bliz-wmo",  ".wmo",  "World model object")
-register("image/vnd.bliz-blp",        ".blp",  "BLP image")
-
 class MimeType(object):
+	_mimes_map = %r
+	_types_map = types_map
+	
 	def __init__(self, mime):
-		if mime not in comments_map:
+		if mime not in self._mimes_map:
 			mime = DEFAULT_MIME_TYPE
 		self.__name = mime
 	
 	@classmethod
 	def fromName(cls, f):
 		_, ext = os.path.splitext(f)
-		if ext not in types_map: # Always try a case-sensitive match first
+		if ext not in cls._types_map: # Always try a case-sensitive match first
 			ext = ext.lower()
-		if ext in types_map:
-			mime = types_map[ext]
+		if ext in cls._types_map:
+			mime = cls._types_map[ext]
 		else:
 			mime = DEFAULT_MIME_TYPE
 		return cls(mime)
 	
+	@classmethod
+	def register(cls, mime, extension, parent="", comment="", icon=""):
+		cls._mimes_map[mime] = (parent, comment, icon)
+		cls._types_map[extension] = mime
+	
+	@classmethod
+	def registerExtension(cls, mime, extension):
+		cls._types_map[extension] = mime
+	
 	def comment(self):
-		return comments_map[self.name()]
+		return self._mimes_map[self.name()][1]
+	
+	def genericIcon(self):
+		return self._mimes_map[self.name()][2]
+	
+	def icon(self):
+		return self.genericIcon() or self.name().replace("/", "-")
+	
+	def isDefault(self):
+		return self.name() is DEFAULT_MIME_TYPE
 	
 	def name(self):
 		return self.__name
+	
+	def parent(self):
+		parent = self._mimes_map[self.name()][0]
+		return MimeType(parent)
+
+del types_map
+assert DEFAULT_MIME_TYPE in MimeType._mimes_map
+
+MimeType.register("application/vnd.bliz-anim", ".anim", "", "Model ANIM data",    "3d-x-generic")
+MimeType.register("application/vnd.bliz-dbc",  ".dbc",  "", "WoW database file",  "x-office-spreadsheet")
+MimeType.register("application/vnd.bliz-mdx",  ".mdx",  "", "Blizzard MDX model", "3d-x-generic")
+MimeType.register("application/vnd.bliz-mpq",  ".mpq",  "", "MPQ archive",        "package-x-generic")
+MimeType.register("application/vnd.bliz-skin", ".skin", "", "Model SKIN data",    "3d-x-generic")
+MimeType.register("application/vnd.bliz-wmo",  ".wmo",  "", "World model object", "3d-x-generic")
+MimeType.register("image/vnd.bliz-blp",        ".blp",  "", "BLP image",          "image-x-generic")
+MimeType.registerExtension("application/vnd.bliz-dbc", ".db2")
+MimeType.registerExtension("application/vnd.bliz-dbc", ".wdb")
+MimeType.registerExtension("application/vnd.bliz-mdx", ".mdx")
+MimeType.registerExtension("application/vnd.bliz-mdx", ".m2")
+MimeType.registerExtension("application/vnd.bliz-mdx", ".m3")
+
+# Python bugs
+MimeType.registerExtension("application/x-font-ttf", ".ttf")
+MimeType.registerExtension("audio/ogg", ".ogg")
 """
+
+def getFirstTagData(element, tag):
+	taglist = element.getElementsByTagName(tag)
+	if taglist:
+		child = taglist[0].firstChild
+		if child:
+			return str(child.data)
+	return ""
 
 def main():
 	if len(sys.argv) < 2:
@@ -61,10 +97,11 @@ def main():
 	
 	out = {}
 	for element in mimetypes:
-		mime = element.getAttribute("type")
-		comments = element.getElementsByTagName("comment")
-		comment = comments[0].firstChild.data
-		out[mime] = comment
+		mime = str(element.getAttribute("type"))
+		parent = getFirstTagData(element, "sub-class-of")
+		comment = getFirstTagData(element, "comment")
+		defaultIcon = getFirstTagData(element, "generic-icon")
+		out[mime] = (parent, comment, defaultIcon)
 	
 	f = open("_mime.py", "w")
 	
