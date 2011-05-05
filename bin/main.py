b2cd1f2754010c3c55667b0f17c34379807c0585
Jerome Leclanche <jerome@leclan.ch>
2011-05-05 03:22:59 +0100

    Use realpath() to append paths

--- bin/main.py
+++ bin/main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir))
+
 if __name__ == "__main__":
-	import os
-	import sys
-	sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir))
 	from mpqt.main import main
 	main()
